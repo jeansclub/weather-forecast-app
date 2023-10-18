026697b6249e87a7162c019e41a7ea04b0c55934
Philip Anwa <ukpaiihechi@rocketmail.com>
2023-10-18 19:47:05 +0100

    Complete forecast app

--- index.js
+++ index.js
@@ -39,6 +39,8 @@ function btnShadowTogle() {
 function srcShadowTogle() {
     var searchInput = document.getElementById("search-location");
     searchInput.classList.toggle("box-shadow");
+
+    document.getElementById("search-location").style.borderStyle = "none";
 }
 
 //-----------------------------------------------------------------------------------------------
@@ -62,12 +64,12 @@ function valueTest() {
 
 async function fectchResult(target) {
     
-    const url = "http://api.weatherapi.com/v1/current.json?key=9b99f40c7b0341bcbe0211219232509&q="+target+"&aqi=no";
+    const url = "http://api.weatherapi.com/v1/forecast.json?key=9b99f40c7b0341bcbe0211219232509&q="+target+"&days=1&aqi=no&alerts=no";
     const res = await fetch(url);
     const data = await res.json();
 
     
-    document.querySelector(".location").innerHTML = data.location.name;
+    document.querySelector(".city").innerHTML = data.location.name + ", ";
     document.querySelector(".time").innerHTML = data.location.localtime;
     document.querySelector(".weather-icon").src = "http:" + data.current.condition.icon;
     document.querySelector(".weather-msg").innerHTML = data.current.condition.text;
@@ -77,6 +79,30 @@ async function fectchResult(target) {
     document.querySelector(".pressure").innerHTML = "Pressure: " + data.current.pressure_mb + " Mb";
     document.querySelector(".humidity").innerHTML = "Humidity: " + data.current.humidity + " %";
     document.querySelector(".wind").innerHTML = "Wind: " + data.current.wind_kph + " Km/H";
+
+    document.querySelector(".region").innerText = data.location.region + ", ";
+    document.querySelector(".country").innerText = data.location.country + ":-";
+
+
+    for (let index = 0; index < document.querySelectorAll(".forecast-time").length; index++) {
+        document.querySelectorAll(".forecast-time")[index].innerText = data.forecast.forecastday[0].hour[index].time;
+    }
+
+    for (let index = 0; index < document.querySelectorAll(".forecast-weather-icon").length; index++) {
+        document.querySelectorAll(".forecast-weather-icon")[index].src = "http:" + data.forecast.forecastday[0].hour[index].condition.icon;
+    }
+
+    for (let index = 0; index < document.querySelectorAll(".forecast-weather-msg").length; index++) {
+        document.querySelectorAll(".forecast-weather-msg")[index].innerText = data.forecast.forecastday[0].hour[index].condition.text;
+    }
+
+    for (let index = 0; index < document.querySelectorAll(".forecast-temp-f").length; index++) {
+        document.querySelectorAll(".forecast-temp-f")[index].innerText = data.forecast.forecastday[0].hour[index].temp_f + "°F";
+    }
+
+    for (let index = 0; index < document.querySelectorAll(".forecast-temp-c").length; index++) {
+        document.querySelectorAll(".forecast-temp-c")[index].innerText = data.forecast.forecastday[0].hour[index].temp_c + "°C";
+    }
 
     console.log(data);
 }
@@ -93,3 +119,16 @@ fectchResult(weatherLocation);
 }
 
 weatherForecast();*/
+
+
+/*async function forecastResult(result) {
+    const forCastUrl = "http://api.weatherapi.com/v1/forecast.json?key=9b99f40c7b0341bcbe0211219232509&q="+target+"&days=1&aqi=no&alerts=no";
+    const respond = await fetch(forCastUrl);
+    const forecastData = await respond.json();
+
+    console.log(forecastData);
+} 
+
+forecastResult(weatherLocation)*/
+
+//http://api.weatherapi.com/v1/current.json?key=9b99f40c7b0341bcbe0211219232509&q="+target+"&aqi=no"
