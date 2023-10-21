4ce15e08854f4a94c668171d7ed3f0186a6bfbef
Philip Anwa <ukpaiihechi@rocketmail.com>
2023-10-21 19:33:40 +0100

    add day name

--- index.js
+++ index.js
@@ -82,6 +82,7 @@ async function fectchResult(target) {
 
     document.querySelector(".region").innerText = data.location.region + ", ";
     document.querySelector(".country").innerText = data.location.country + ":-";
+    document.querySelector(".days").innerText = getDayName(new Date().getDay());
 
 
     for (let index = 0; index < document.querySelectorAll(".forecast-time").length; index++) {
@@ -107,3 +108,30 @@ async function fectchResult(target) {
 }
 
 fectchResult(weatherLocation);
+
+
+function getDayName(number) {
+    switch (number) {
+        case 0:
+            
+            return "Sunday";
+        case 1:
+            
+            return "Monday";
+        case 2:
+            
+            return "Tuesday";
+        case 3:
+            
+            return "Wednesday";
+        case 4:
+            
+            return "Thursday";
+        case 5:
+            
+            return "Friday";
+        case 6:
+            
+            return "Saturday";
+    }
+}
